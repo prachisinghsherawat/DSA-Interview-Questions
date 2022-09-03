8eeb66f4bf7447f8c775555bf1c057e5ee24f436
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-09-03 21:29:09 +0530

    question 5

---
+++ question5.js
@@ -0,0 +1,21 @@
+
+
+let arr = ["priya", 0 ,"", false, null,undefined, "ate", NaN ,9 ]
+
+//output = ["priya","ate",9];
+
+let bag = [];
+
+for(let i=0 ;i<arr.length ;i++){
+  if(arr[i]){
+    bag.push(arr[i])
+  }
+}
+
+
+console.log(bag)
+
+
+let valu =11;
+
+5%3==0 ? console.log("yes"): 11%5==0 ? console.log("some") : console.log("prachu")
