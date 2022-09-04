e9732bc6d0966e86ef4aefd6ed247d20375fbcf7
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-09-04 22:13:37 +0530

    find method

---
+++ Inbuilt Functions/Find.js
@@ -0,0 +1,14 @@
+
+
+// Find() --->  find function returns the value of first element that passes the test 
+
+
+
+let arr = [5,12,8,130,44];
+
+let num = arr.find((el)=> el>10);
+
+console.log(num)
+
+
+// OUTPUT  ---> 20
