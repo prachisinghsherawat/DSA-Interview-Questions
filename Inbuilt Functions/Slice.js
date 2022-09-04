0cb962e1609f4375fb50aaf05373a4fb88ae5703
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-09-04 23:28:04 +0530

    slice method updated

---
+++ Inbuilt Functions/Slice.js
@@ -0,0 +1,22 @@
+
+
+// SLICE() METHOD ----->
+
+
+// The slice() method returns selected elements in an array, as a new array.
+// The slice() method selects from a given start, up to a (not inclusive) given end.
+// The slice() method does not change the original array.
+
+
+
+let arr = [1,2,3,4,5]
+
+let a = arr.slice(0,4)
+
+console.log(a)
+
+
+
+// OUTPUT -----> [ 1 , 2, 6 , 7 ]
+
+
