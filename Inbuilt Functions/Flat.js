2dd3c458aea289a124b2b3481237dd68b47850e1
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-09-04 22:22:00 +0530

    flat method updated

---
+++ Inbuilt Functions/Flat.js
@@ -0,0 +1,13 @@
+
+
+// Flat() ------> Flat method creates a new array with all sub-array elements concatenated into it recursively up to the specified depth
+
+
+// Flatten Array 
+
+let arr = [1,2,[1,2,3,4],[[1,2]]];
+
+console.log(arr.flat(2))
+
+
+// OUTPUT is ----> [1,2,1,2,3,4,1,2]
