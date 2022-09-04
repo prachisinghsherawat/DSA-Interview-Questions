8e93be837d9d1ed452dad61b15e36b454c0a9428
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-09-04 23:25:34 +0530

    splice method updated

---
+++ Inbuilt Functions/Splice.js
@@ -0,0 +1,26 @@
+
+
+
+// SPLICE() METHOD ----------->
+
+
+// The splice() method adds and/or removes array elements.
+// The splice() method overwrites the original array.
+
+
+
+let arr = [1,2,3,4,5]
+
+
+arr.splice(2,0,6,7,9)
+
+console.log(arr)
+
+
+// OUTPUT -----> [ 1 , 2 , 6 , 7 , 9 , 3 , 4 , 5 ]
+
+
+
+
+
+
