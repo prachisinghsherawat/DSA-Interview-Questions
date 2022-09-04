a959f288101a3acd8d1b830337fcec92739b1084
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-09-04 22:36:33 +0530

    concat method updated

--- Inbuilt Functions/Concat.js
+++ Inbuilt Functions/Concat.js
@@ -2,8 +2,15 @@
 
 // CONCAT() METHOD ---->  
 
-// concat function is used to join two or more strings together in javascript .
 
+
+// The concat() method concatenates (joins) two or more arrays.
+
+// The concat() method returns a new array, containing the joined arrays.
+
+// The concat() method does not change the existing arrays.
+
+// the concat() method use to join two or mor strings together.
 
 
 let arr = [1,2,[1,2,3,4],[[1,2]]];
