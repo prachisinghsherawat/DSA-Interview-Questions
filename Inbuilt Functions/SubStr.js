a40b044a6c41d022b63395ee75b1c1d9e5ecfbae
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-09-04 23:30:56 +0530

    slice method updated

---
+++ Inbuilt Functions/SubStr.js
@@ -0,0 +1,28 @@
+
+
+
+// SUBSTR() METHOD --------->
+
+
+// The substr() method extracts a part of a string.
+// The substr() method begins at a specified position, and returns a specified number of characters.
+// The substr() method does not change the original string.
+
+
+
+let obj = {
+  
+    firstName : "Prachi" ,
+    lastName : "Singh" 
+}
+  
+let a = obj.firstName.substr(0,3)
+let b = obj.lastName.substr(2)
+  
+console.log(a+b)
+
+
+// OUTPUT ------> Prangh
+
+
+
