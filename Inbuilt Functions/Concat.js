bebffc19fc0f39f5fc09200e09a39e032c51780d
Prachi Singh Sherawat <96073111+prachisinghsherawat@users.noreply.github.com>
2022-09-15 21:43:12 +0530

    Update Concat.js

--- Inbuilt Functions/Concat.js
+++ Inbuilt Functions/Concat.js
@@ -3,7 +3,6 @@
 // CONCAT() METHOD ---->  
 
 
-
 // The concat() method concatenates (joins) two or more arrays.
 // The concat() method returns a new array, containing the joined arrays.
 // The concat() method does not change the existing arrays.
